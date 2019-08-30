36b2e24b680925ffd1b56e3a64f11d49db725a98
Gerard KESSE <tiakagerard@hotmail.com>
2019-08-30 08:27:18 +0200

    Initial Commit

--- mingw/cmd_install_lib.bat
+++ mingw/cmd_install_lib.bat
@@ -1,103 +1,16 @@
 @echo off
 ::===============================================
+set "GLIB_NAME=%1%"
 ::===============================================
-set "GLIB_BUILD=C:\lib\build"
-set "GLIB_DEV=C:\Users\Admin\Downloads\Programs\ReadyLib\dev"
-set "GCYGWIN=C:\cygwin64\bin"
+if "%GLIB_NAME%" == "" ( set "GLIB_NAME=all")
 ::===============================================
-set "GLIB_SCRIPT_ROOT=%cd%"
-set "GLIB_CONFIG_ROOT=%GLIB_SCRIPT_ROOT%\config"
-set "GLIB_MAKEFILE_ROOT=%GLIB_SCRIPT_ROOT%\makefile"
-::===============================================
-call :GLib_Generate glm
-goto :eof 
-::===============================================
-:GLib_Create
-    echo [ INFO ] Creation des repertoires lib...[ lib + build + dev ]
-    if not exist %GLIB_BUILD% ( mkdir %GLIB_BUILD% )
-    if not exist %GLIB_DEV% ( mkdir %GLIB_DEV% )
-    
-    echo GLIB_BUILD : %GLIB_BUILD%
-    echo GLIB_DEV : %GLIB_DEV%
-    echo GCYGWIN : %GCYGWIN%
-    echo.
-goto :eof
-::===============================================
-:GLib_Config
-    echo [ INFO ] configuration de la librairie...[ %GLIB_NAME% ]
-    set "GLIB_BUILD_NAME=%GLIB_BUILD%\%GLIB_NAME%"
-    set "GLIB_DEV_NAME=%GLIB_DEV%\%GLIB_NAME%"
-    set "GLIB_DEV_DIR=%GLIB_DEV_NAME%\%GLIB_ARCH%"
-    set "GLIB_BUILD_SRC=%GLIB_BUILD_NAME%\source"
-    set "GLIB_BUILD_DIR=%GLIB_BUILD_NAME%\build"
-    set "GLIB_BUILD_PREFIX=%GLIB_BUILD_NAME%\install"
-    set "GLIB_CONFIG_NAME=config_%GLIB_NAME%.bat"
-    set "GLIB_CONFIG_PATH=%GLIB_CONFIG_ROOT%\%GLIB_CONFIG_NAME%"
-    set "GLIB_MAKEFILE_NAME=makefile_%GLIB_NAME%.txt"
-    set "GLIB_MAKEFILE_PATH=%GLIB_MAKEFILE_ROOT%\%GLIB_MAKEFILE_NAME%"
-    
-    echo GLIB_NAME : %GLIB_NAME%
-    echo GLIB_SCRIPT_ROOT : %GLIB_SCRIPT_ROOT%
-    echo GLIB_CONFIG_ROOT : %GLIB_CONFIG_ROOT%
-    echo GLIB_CONFIG_NAME : %GLIB_CONFIG_NAME%
-    echo GLIB_CONFIG_PATH : %GLIB_CONFIG_PATH%
-    echo.
-
-    call %GLIB_CONFIG_PATH%
-goto :eof
-::===============================================
-:GLib_Download
-    echo [ INFO ] telechargement de la librairie...[ %GLIB_NAME% ]
-    echo GLIB_LINK : %GLIB_LINK%
-    echo GLIB_BUILD_SRC : %GLIB_BUILD_SRC%
-    echo GLIB_VERSION_ARCHIVE : %GLIB_VERSION_ARCHIVE%
-    echo GLIB_VERSION : %GLIB_VERSION%
-    echo GLIB_ARCHIVE_NAME : %GLIB_ARCHIVE_NAME%
-    echo GLIB_VERSION_NAME : %GLIB_VERSION_NAME%
-    echo.
-    
-    if not exist %GLIB_BUILD_SRC% ( mkdir %GLIB_BUILD_SRC% )
-    cd %GLIB_BUILD_SRC%
-    %myvar%
-    cd %GLIB_SCRIPT_ROOT%
-goto :eof
-::===============================================
-:GLib_Build
-    echo "[ INFO ] generation de la librairie...[ $GLIB_NAME ]"
-    mkdir -p $GLIB_BUILD_DIR
-    cd $GLIB_BUILD_DIR
-    eval $GLIB_BUILD_RUN
-goto :eof
-::===============================================
-:GLib_Make
-    echo "[ INFO ] construction de la librairie...[ $GLIB_NAME ]"
-    cd $GLIB_BUILD_DIR
-    eval $GLIB_BUILD_MAKE
-goto :eof
-::===============================================
-:GLib_Install
-    echo "[ INFO ] installation de la librairie...[ $GLIB_NAME ]"
-    mkdir -p $GLIB_DEV_DIR
-    cd $GLIB_DEV_DIR
-    eval $GLIB_DEV_INSTALL
-goto :eof
-::===============================================
-:GLib_Clean
-    echo "[ INFO ] suppression du repertoire build de la librairie...[ $GLIB_NAME ]"
-    #rm -rf $GLIB_BUILD_SRC
-    #rm -rf $GLIB_BUILD_DIR
-    #rm -rf $GLIB_BUILD
+call :GLib_Generate
 goto :eof
 ::===============================================
 :GLib_Generate
-    set "GLIB_NAME=%1"
-    call :GLib_Create
-    call :GLib_Config
-    call :GLib_Download
-goto :eof
-::===============================================
-:GLib_Arch
-set GLIB_ARCH=mingw
-    if exist "%PROGRAMFILES(X86)%" ( set GLIB_ARCH=mingw64 )
+    cd config
+    set "GLIB_INSTALL_LIB=cmd_install_%GLIB_NAME%.bat"
+    call %GLIB_INSTALL_LIB%
+    cd ..
 goto :eof
 ::===============================================
---
+++ mingw/config/cmd_install_sdl.bat
@@ -0,0 +1,83 @@
+@echo off
+::===============================================
+set PATH=C:\MinGW\bin;%PATH%
+set PATH=C:\Program Files\CMake\bin;%PATH%
+set "GCYGWIN=C:\cygwin64\bin"
+::===============================================
+set "GLIB_NAME=sdl"
+::===============================================
+set "GLIB_BUILD=C:\lib\build"
+set "GLIB_DEV=C:\Users\Admin\Downloads\Programs\ReadyLib\dev"
+::===============================================
+set "GLIB_SCRIPT_ROOT=%cd%"
+::===============================================
+call :GLib_Arch
+::===============================================
+set "GLIB_BUILD_NAME=%GLIB_BUILD%\%GLIB_NAME%"
+set "GLIB_BUILD_SRC=%GLIB_BUILD_NAME%\source"
+set "GLIB_BUILD_DIR=%GLIB_BUILD_NAME%\build"
+set "GLIB_BUILD_PREFIX=%GLIB_BUILD_NAME%\install"
+set "GLIB_DEV_NAME=%GLIB_DEV%\%GLIB_NAME%"
+set "GLIB_DEV_DIR=%GLIB_DEV_NAME%\%GLIB_ARCH%"
+::===============================================
+set "GLIB_LINK=https://www.libsdl.org/release/SDL2-2.0.10.zip"
+set "GLIB_ARCHIVE_NAME=SDL2-2.0.10"
+set "GLIB_SOURCE_DIR=%GLIB_BUILD_SRC%\%GLIB_ARCHIVE_NAME%"
+::===============================================
+call :GLib_Generate
+goto :eof
+::===============================================
+:GLib_Download
+    echo [ INFO ] Telechargement de la librairie...[ %GLIB_NAME% ]
+    if not exist %GLIB_BUILD_SRC% ( mkdir %GLIB_BUILD_SRC% )
+    cd %GLIB_BUILD_SRC%
+    %GCYGWIN%\wget %GLIB_LINK%
+    %GCYGWIN%\unzip %GLIB_ARCHIVE_NAME%
+    cd %GLIB_SCRIPT_ROOT%
+goto :eof
+::===============================================
+:GLib_Build
+    echo [ INFO ] Generation de la librairie...[ %GLIB_NAME% ]
+    if not exist %GLIB_BUILD_DIR% ( mkdir %GLIB_BUILD_DIR% )
+    cd %GLIB_BUILD_DIR%
+    cmake -G "MinGW Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=%GLIB_BUILD_PREFIX% ^
+    %GLIB_SOURCE_DIR%
+    cd %GLIB_SCRIPT_ROOT%
+goto :eof
+::===============================================
+:GLib_Make
+    echo [ INFO ] Construction de la librairie...[ %GLIB_NAME% ]
+    cd %GLIB_BUILD_DIR%
+    mingw32-make
+    mingw32-make install
+    cd %GLIB_SCRIPT_ROOT%
+goto :eof
+::===============================================
+:GLib_Install
+    echo [ INFO ] Installation de la librairie...[ %GLIB_NAME% ]
+    if not exist %GLIB_DEV_DIR% ( mkdir %GLIB_DEV_DIR% )
+    cd %GLIB_DEV_DIR%
+    if not exist "..\include" ( mkdir "..\include" )
+    if not exist ".\lib" ( mkdir ".\lib" )
+    echo. > ./README.md
+    echo. > ../README.md
+    echo. > ../../README.md
+    if exist "%GLIB_BUILD_PREFIX%\include" ( xcopy /q /s /i /y "%GLIB_BUILD_PREFIX%\include" "..\include" )
+    if exist "%GLIB_BUILD_PREFIX%\lib" ( xcopy /q /s /i /y "%GLIB_BUILD_PREFIX%\lib" ".\lib" )
+    if exist "%GLIB_BUILD_PREFIX%\lib64" ( xcopy /q /s /i /y "%GLIB_BUILD_PREFIX%\lib64" ".\lib" )
+    cd %GLIB_SCRIPT_ROOT%
+goto :eof
+::===============================================
+:GLib_Arch
+set "GLIB_ARCH=mingw"
+    if exist "%PROGRAMFILES(X86)%" ( set "GLIB_ARCH=mingw64" )
+goto :eof
+::===============================================
+:GLib_Generate
+    ::call :GLib_Download
+    call :GLib_Build
+    call :GLib_Make
+    call :GLib_Install
+goto :eof
+::===============================================
